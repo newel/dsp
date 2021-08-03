10099d2e8edc834c8827d69a7d879762e63de989
newel <guzhuopiao@hotmail.com>
2021-08-03 18:04:40 +0800

    init

---
+++ config_dialog.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'e:\dsp\config_dialog.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(419, 249)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(Dialog)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        self.comboBox_village = QtWidgets.QComboBox(Dialog)
+        self.comboBox_village.setObjectName("comboBox_village")
+        self.comboBox_village.addItem("")
+        self.comboBox_village.addItem("")
+        self.gridLayout.addWidget(self.comboBox_village, 1, 1, 1, 1)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 2, 0, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        self.comboBox_dsp_type = QtWidgets.QComboBox(Dialog)
+        self.comboBox_dsp_type.setObjectName("comboBox_dsp_type")
+        self.comboBox_dsp_type.addItem("")
+        self.comboBox_dsp_type.addItem("")
+        self.comboBox_dsp_type.addItem("")
+        self.gridLayout.addWidget(self.comboBox_dsp_type, 0, 1, 1, 1)
+        self.input_reserve = QtWidgets.QLineEdit(Dialog)
+        self.input_reserve.setObjectName("input_reserve")
+        self.gridLayout.addWidget(self.input_reserve, 3, 1, 1, 1)
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setObjectName("label_4")
+        self.gridLayout.addWidget(self.label_4, 1, 0, 1, 1)
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 0, 0, 1, 1)
+        self.input_ip = QtWidgets.QLineEdit(Dialog)
+        self.input_ip.setObjectName("input_ip")
+        self.gridLayout.addWidget(self.input_ip, 2, 1, 1, 1)
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 3, 0, 1, 1, QtCore.Qt.AlignHCenter)
+        self.button_bind = QtWidgets.QPushButton(Dialog)
+        self.button_bind.setObjectName("button_bind")
+        self.gridLayout.addWidget(self.button_bind, 4, 1, 1, 1)
+        self.horizontalLayout.addLayout(self.gridLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "打开"))
+        self.comboBox_village.setItemText(0, _translate("Dialog", "一小区"))
+        self.comboBox_village.setItemText(1, _translate("Dialog", "二小区"))
+        self.label.setText(_translate("Dialog", "IP"))
+        self.comboBox_dsp_type.setItemText(0, _translate("Dialog", "B4860"))
+        self.comboBox_dsp_type.setItemText(1, _translate("Dialog", "BSC9132"))
+        self.comboBox_dsp_type.setItemText(2, _translate("Dialog", "5G"))
+        self.label_4.setText(_translate("Dialog", "小区规格"))
+        self.label_3.setText(_translate("Dialog", "芯片类型"))
+        self.label_2.setText(_translate("Dialog", "保留"))
+        self.button_bind.setText(_translate("Dialog", "Bind"))
---
+++ core_detail.py
@@ -0,0 +1,63 @@
+
+import Ui_core_detail_window
+
+import sys
+from PyQt5.QtWidgets import *
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+
+class Core_detail(QtWidgets.QMainWindow,Ui_core_detail_window.Ui_MainWindow):
+    
+    def __init__(self, parent =None):
+        super(Core_detail, self).__init__(parent)
+        self.setupUi(self)
+        self.setWindowTitle('此处为标题')
+        self.treeWidget_variable_init()
+        self.bindTriggered()
+
+
+    def treeWidget_variable_init(self):
+        '''
+            初始化变量数据
+        '''        
+        parent = None
+        for x in range(1,4097):      
+            if parent == None:
+                parent=QTreeWidgetItem(self.treeWidget_variable)
+                parent.setText(0,'变量名称'+str(x))
+                parent.setText(1,'变量值'+str(x))
+                parent.setText(2,'变量类型'+str(x))
+                parent.setText(3,'地址'+str(x))
+                parent.setText(4,'显示模式'+str(x))
+            else:
+                child1=QTreeWidgetItem(parent)
+                child1.setText(0,'变量名称'+str(x))
+                child1.setText(1,'变量值'+str(x))
+                child1.setText(2,'变量类型'+str(x))
+                child1.setText(3,'地址'+str(x))
+                child1.setText(4,'显示模式'+str(x))
+                parent = child1
+        self.treeWidget_variable.expandAll()
+
+    def bindTriggered(self):
+        '''
+            事件绑定
+        '''
+        self.treeWidget_variable.clicked.connect(self.treeWidget_variable_triggered)
+        self.button_refresh.clicked.connect(self.treeWidget_variable_triggered)
+
+    def treeWidget_variable_triggered(self):
+        print('treeWidget_variable triggered')
+
+        
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv) 
+    mainWindow = Core_detail()
+    mainWindow.show()
+    sys.exit(app.exec_())
+    
