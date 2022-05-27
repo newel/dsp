aac4656094e2565842504f0a50850c7dd6ed27b7
newel <guzhuopiao@hotmail.com>
2022-05-27 09:34:41 +0800

    a

--- guangxi_project/merge.py
+++ guangxi_project/merge.py
@@ -2,7 +2,30 @@
 #  -*- coding: utf-8 -*-
 __author__ = 'lxr'
 
+import pandas as pd
+from soupsieve import select
+
 
 if __name__ == '__main__':
-    pass
+    config_df = pd.read_excel('./config.xlsx') 
+    result_column = None
+    result_df = pd.DataFrame()
+    for index,row in config_df.iterrows():    
+        if row['type'] == 'result':
+            result_column = row['value'].split(',')
+        elif row['type'] == 'file_column':
+            file_df = pd.read_excel(row['key'])
+            value = str(row['value'])
+            select_column = value.split(",")
+            file_df = file_df[row['value'].split(",")]
+            file_df.columns = result_column
+            if result_df.empty:
+                result_df = file_df
+            else:
+                result_df = pd.concat([result_df,file_df]) 
+            # print(file_df.head())
+            
+    print(result_df.head())
+    print(result_df.tail())
+    # result_df.to_excel('merge.xlsx',index = False)
     
