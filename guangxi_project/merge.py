cc4ad2035433616bd374d6cc54694d9bf4c82ebd
newel <guzhuopiao@hotmail.com>
2022-05-27 17:23:02 +0800

    a

--- guangxi_project/merge.py
+++ guangxi_project/merge.py
@@ -2,6 +2,7 @@
 #  -*- coding: utf-8 -*-
 __author__ = 'lxr'
 
+from numpy import NaN
 import pandas as pd
 from soupsieve import select
 
@@ -14,18 +15,44 @@ if __name__ == '__main__':
         if row['type'] == 'result':
             result_column = row['value'].split(',')
         elif row['type'] == 'file_column':
-            file_df = pd.read_excel(row['key'])
-            value = str(row['value'])
-            select_column = value.split(",")
-            file_df = file_df[row['value'].split(",")]
-            file_df.columns = result_column
-            if result_df.empty:
-                result_df = file_df
+            filename = row['key']
+            if row['sheet'] != -1:
+                file_df = pd.read_excel(filename,sheet_name=row['sheet'])
             else:
-                result_df = pd.concat([result_df,file_df]) 
-            # print(file_df.head())
+                file_df = pd.read_excel(filename)
+            file_df['EnbType'] = 0
+            file_df['NoX2'] = 1
+            file_df['PLMNID'] = 46011
+            file_df['LTENeighborCellId'] = 1
             
-    print(result_df.head())
-    print(result_df.tail())
-    # result_df.to_excel('merge.xlsx',index = False)
+            if 'HC4G工参' in filename:
+                pass   
+            elif 'L网基础工参数据库' in filename:
+                file_df['EnbType'] = file_df.apply(lambda x : 0 if x['小区覆盖类型'] == '宏站' else 1 ,axis=1)
+            elif '诺基亚' in filename:
+                file_df['EnbType'] = file_df.apply(lambda x : 0 if x['站点类型'] == '宏站' else 1 ,axis=1) 
+            elif '京信' in filename:
+                pass                           
+             
+            value = str(row['value'])
+            select_column = value.split(",")            
+            file_df = file_df[select_column]
+            file_df.columns = result_column
+            for x in range(1,33):
+                file_df["pre_neName_"+str(x)] = file_df['neName'].shift(x,axis=0)     
+            for x in range(1,33):
+                file_df['LTENeighborCellId'] = file_df.apply(lambda item : 
+                        x + 1 if  item['neName'] == item['pre_neName_'+str(x)] else item['LTENeighborCellId'],axis =1)
+            # file_df.columns = result_column
+            rtn_df = file_df[result_column]
+            if result_df.empty:
+                result_df = rtn_df
+            else:
+                result_df = pd.concat([result_df,rtn_df]) 
+    
+    result_df['小区名称'] = result_df.apply(lambda x : x['neName']+str(x['CID']) ,axis=1)
+    result_df['mod3'] = result_df.apply(lambda x : x['PCI'] % 3  ,axis=1)
+    # print(result_df.head())
+    # print(result_df.tail())
+    result_df.to_excel('merge.xlsx',index = False)
     
